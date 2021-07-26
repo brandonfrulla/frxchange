9e5bef5ac7911083bdd90be579a688a5e9d02b8e
brandonfrulla <bfrulla@mail.umw.edu>
2021-07-26 15:39:01 -0400

    new file:   migrations/1_initial_migration.js 	new file:   migrations/2_deploy_contracts.js 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   public/manifest.json 	new file:   src/abis/Frxchange.json 	new file:   src/abis/Migrations.json 	new file:   src/abis/Token.json 	new file:   src/abis/frxchange.json 	new file:   src/components/App.css 	new file:   src/components/App.js 	new file:   src/contracts/Frxchange.sol 	new file:   src/contracts/Migrations.sol 	new file:   src/contracts/Token.sol 	new file:   src/index.js 	new file:   src/logo.png 	new file:   src/serviceWorker.js 	new file:   truffle-config.js

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,17 @@
+
+const Token = artifacts.require("Token");
+const Frxchange = artifacts.require("Frxchange");
+
+module.exports = async function(deployer) {
+  //deploy token
+  await deployer.deploy(Token);
+  const token = await Token.deployed()
+
+  //deploy exchange
+  await deployer.deploy(Frxchange);
+  const frxchange = await Frxchange.deployed()
+
+  //transfer to exchange
+  await token.transfer(frxchange.address, '1000000000000000000000000')
+
+};
