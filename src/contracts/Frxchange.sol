f06979c436b69e2a5ffa66f3ba72e38b9292efda
brandonfrulla <bfrulla@mail.umw.edu>
2021-07-28 08:33:02 -0400

    finalizing smart contract basics

--- src/contracts/Frxchange.sol
+++ src/contracts/Frxchange.sol
@@ -8,7 +8,14 @@ contract Frxchange {
     Token public token;
     uint public rate = 100;
     
-    event TokenPurchased(
+    event TokensPurchased(
+        address account,
+        address token,
+        uint amount,
+        uint rate
+    );
+
+    event TokensSold(
         address account,
         address token,
         uint amount,
@@ -20,13 +27,37 @@ contract Frxchange {
     }
 
     function buyTokens() public payable {
+        //calculate # of tokens to trade for 1 eth
         uint tokenAmount = msg.value * rate;
 
+        //ensure exchange token balance can facilitate trade
         require(token.balanceOf(address(this)) >= tokenAmount);
 
+        //execute trade
         token.transfer(msg.sender, tokenAmount);
-    
-        emit TokenPurchased(msg.sender, address(token), tokenAmount, rate);
+
+        //publish event logs for trade 
+        emit TokensPurchased(msg.sender, address(token), tokenAmount, rate);
+    }
+
+    function sellTokens(uint _amount) public payable {
+        //ensurte msg.sender has enough tokens
+        require(token.balanceOf(msg.sender) >= _amount);
+
+        //calculate # of eth to trade for tokens
+        uint ethAmount = _amount / rate;
+
+        //ensure exchange eth balance can facilitate trade
+        require(ethAmount <= address(this).balance);
+
+        
+        //execute trade
+        token.transferFrom(msg.sender, address(this), _amount);
+        msg.sender.transfer(ethAmount);
+
+        //publish event logs for trade
+        emit TokensSold(msg.sender, address(token), _amount, rate);
+
     }
 
 }
--- test/frxchange.test.js
+++ test/frxchange.test.js
@@ -46,7 +46,7 @@ contract(Frxchange,([deployer, investor]) => {
         })
     })
 
-    describe('Token functionality', async () => {
+    describe('Token buy functionality', async () => {
         let result 
 
         before(async () =>{
@@ -57,15 +57,19 @@ contract(Frxchange,([deployer, investor]) => {
         })
 
         it('FRXT purchase from frxchange test', async () => {
+            //verify investor recieved tokens
             let investorBalance = await token.balanceOf(investor)
             assert.equal(investorBalance, tokens('100'))
 
+            //verify exchange token balance decrements
             let excg_token_balance = await token.balanceOf(frxchange.address)
             assert.equal(excg_token_balance, tokens('999900'))
 
+            //verify exchange eth balance increments
             let excg_eth_balance = await web3.eth.getBalance(frxchange.address)
             assert.equal(excg_eth_balance, web3.utils.toWei('1', 'ether'))
 
+            //test logs to verify 
             const event = result.logs[0].args
             assert.equal(event.account, investor)
             assert.equal(event.token, token.address)
@@ -74,7 +78,39 @@ contract(Frxchange,([deployer, investor]) => {
         })
     })
 
+    describe('Token sell functionality', async () => {
+        let result 
 
+        before(async () =>{
+            await token.approve(frxchange.address, tokens('100'), {from: investor})
+
+            result = await frxchange.sellTokens(tokens('100'), {from: investor})
+        })
+
+        it('FRXT sell to exchange test', async () => {
+            //verify investor balance went down by n* rate - 1-> n in this test
+            let investorBalance = await token.balanceOf(investor)
+            assert.equal(investorBalance, tokens('0'))
+
+            //verify echange got n tokens back
+            let excg_token_balance = await token.balanceOf(frxchange.address)
+            assert.equal(excg_token_balance, tokens('1000000'))
+
+            //verify echange eth -1 eth
+            let excg_eth_balance = await web3.eth.getBalance(frxchange.address)
+            assert.equal(excg_eth_balance, web3.utils.toWei('0', 'ether'))
+
+            //test logs to verify 
+            const event = result.logs[0].args
+            assert.equal(event.account, investor)
+            assert.equal(event.token, token.address)
+            assert.equal(event.amount.toString(), tokens('100').toString())
+            assert.equal(event.rate.toString(), '100')
+
+            //FAIL; selling more tokens than in wallet
+            await frxchange.sellTokens(tokens('500'), {from: investor}).should.be.rejected;
+        })
+    })
 
 })
     
